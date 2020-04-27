cbb966d9c2d449d00c2d9fd3541ee488e42cf776
LilianSeon <eragnylilian@live.fr>
2020-04-27 11:26:00 +0200

    Compte Bancaire

---
+++ CompteBancaire/CompteBancaire.py
@@ -0,0 +1,21 @@
+class CompteBancaire:
+    def __init__(self, nom, solde):
+        self.nom = nom
+        self.solde = solde
+    def depot(self, somme):
+        self.solde = self.solde + somme
+    def retrait(self, somme):
+        if(somme > self.solde):
+            print("Solde insuffisant")
+        else:
+            self.solde = self.solde - somme
+    def affiche(self):
+        nom = self.nom
+        solde = self.solde
+        text = "Le solde du compte bancaire de {} est de {}€"
+        print(text.format(nom, solde))
+
+compte1 = CompteBancaire("Duchmol", 1000)
+compte1.depot(350)
+compte1.retrait(200)
+compte1.affiche() 
